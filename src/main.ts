35d6246dff4aa17d93a88219c13c6b8fdbc45693
labrac <raquelortegalabrac@hotmail.com>
2023-05-10 20:12:08 +0200

    Add typescript configuration and function

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+const fizzbuzz = (start: number, end: number) => {
+  const fizzbuzzArray = [];
+  for (let index = start; index <= end; index++) {
+    if (index % 3 === 0 && index % 5 === 0) {
+      fizzbuzzArray.push("fizzbuzz");
+    } else if (index % 3 === 0) {
+      fizzbuzzArray.push("fizz");
+    } else if (index % 5 === 0) {
+      fizzbuzzArray.push("buzz");
+    } else {
+      fizzbuzzArray.push(index);
+    }
+  }
+  console.log(fizzbuzzArray);
+};
+
+fizzbuzz(2, 15);
